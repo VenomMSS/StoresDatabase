13a5ac970f1c01078df9754d440ac7152f08676d
Gerard O'Toole <otoolegerard@gmail.com>
2019-05-02 13:08:21 +0100

    Setting up file open and flowdocument

--- StoresDatabase/MainWindow.xaml.cs
+++ StoresDatabase/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,87 @@ namespace StoresDatabase
     /// </summary>
     public partial class MainWindow : Window
     {
+        OpenFileDialog openFileDlg;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FileOpen_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // OPen a file and read the contents
+            // Firstly need a Openfile dialog
+            openFileDlg = new OpenFileDialog();
+            openFileDlg.FileOk += openCSVFile;
+            openFileDlg.Title = "Open File";
+            openFileDlg.Filter = "CSV Files(*.csv)|*.csv|Text file(*.txt)|*.txt|All Files(*.*)|*.*";
+
+            openFileDlg.ShowDialog();
+
+        }
+
+        private void openTextFile(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string fullPathname = openFileDlg.FileName;
+            Paragraph para;
+            FileInfo src = new FileInfo(fullPathname);
+
+            string line;
+            
+           TextReader reader = src.OpenText();
+
+            // read first line containing the name of event
+            line = reader.ReadLine();
+            
+            while (line != null)
+            {
+                para = new Paragraph();
+                para.Inlines.Add(line + '\n'+'\r');
+                string[] fields = line.Split(' ');
+                foreach (string f in fields)
+                {
+                    para.Inlines.Add(f + '\n');
+                }
+                para.Inlines.Add(" " + '\n' + '\r');
+                ViewDoc.Blocks.Add(para);
+                // read next line
+                line = reader.ReadLine();
+
+            }
+            reader.Close();
+
+        }
+
+        private void openCSVFile(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string fullPathname = openFileDlg.FileName;
+            Paragraph para;
+            FileInfo src = new FileInfo(fullPathname);
+
+            string line;
+
+            TextReader reader = src.OpenText();
+
+            // read first line containing the name of event
+            line = reader.ReadLine();
+
+            while (line != null)
+            {
+                para = new Paragraph();
+                para.Inlines.Add(line + '\n' + '\r');
+                string[] fields = line.Split(',');
+                foreach (string f in fields)
+                {
+                    para.Inlines.Add(f + '\n');
+                }
+                para.Inlines.Add(" " + '\n' + '\r');
+                ViewDoc.Blocks.Add(para);
+                // read next line
+                line = reader.ReadLine();
+
+            }
+            reader.Close();
+
         }
     }
 }
