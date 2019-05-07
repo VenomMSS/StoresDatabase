692d41269b34fb3e003c9c4f936d1ab36bd6d51a
Gerard O'Toole <otoolegerard@gmail.com>
2019-05-07 12:24:40 +0100

    Added StockDBClasses and ok

--- StoresDatabase/MainWindow.xaml.cs
+++ StoresDatabase/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SQLite;
+using StockDBClasses;
+
 
 namespace StoresDatabase
 {
@@ -27,7 +29,17 @@ namespace StoresDatabase
         SQLiteConnection database;
         SQLiteDataAdapter partsAdapter, locationAdapter, supplierAdapter;
         String Connection;
+        Item part;
+        Location location;
+        Supplier supplier;
+        ItemType partType;
+        
 
+        /// IMPORTANT SQLITE NOTE
+        /// IT IS ABSOLUTELY ESSENTIAL THAT THE TABLE AND FIELD VALUE
+        /// ONLY CONTAIN A-Z AND 1-0
+        /// SPACES, PUNCTUATION ETC WILL CAUSE A FAIL IN CREATING THE TABLE
+        
         // Database tables
         private static String table_parts = "Item";
         private static String table_parttype = "ItemType";
@@ -37,6 +49,7 @@ namespace StoresDatabase
         // table_part fields
         private static String field_partName = "Name";
         private static String field_PartDescription = "Description";
+        private static String field_SupplierPartNo = "PartNo";
         private static String field_stock = "Amount";
         private static String field_partUnit = "Unit";
         private static String field_price = "Price";
@@ -47,15 +60,16 @@ namespace StoresDatabase
         private static String field_status = "Status";
 
         // table_location fields
-        private static String field_locName = "LocationName";
-        private static String field_locType = "LocationType";
+        private static String field_locName = "Location";
+        private static String field_locType = "Type";
         private static String field_InLocationFK = "LocationFK";
 
         // table_supplier fields
-        private static string field_SupName = "SupplierName";
-        private static string field_SupAddress = "SupplierAddress";
-        private static string field_Supemail = "SupplierEmail";
-        private static string field_SupTel = "SupplierTel";
+        private static string field_SupName = "Supplier";
+        private static string field_SupAddress = "Address";
+        private static string field_Supwebsite = "Web";
+        private static string field_Supemail = "Email";
+        private static string field_SupTel = "Tel";
 
         // table_parttype fields
         private static String field_parttype = "ItemType";
@@ -162,8 +176,9 @@ namespace StoresDatabase
             // Create the parts table
             Cmd.CommandText = "CREATE TABLE IF NOT EXISTS " + table_parts +
                 " (partID integer primary key, " + field_partName + " TEXT, " +
-                  field_PartDescription + " TEXT, " +  field_partUnit + " TEXT, " + 
-                  field_stock + " INTEGER, " +  field_price + " FLOAT, " + field_currency + " TEXT, " +
+                  field_PartDescription + " TEXT, " +  field_partUnit + " TEXT, " +
+                  field_SupplierPartNo + " TEXT, " + field_stock + " INTEGER, " + 
+                  field_price + " FLOAT, " + field_currency + " TEXT, " +
                   field_LocFK + " INTEGER, " + field_partTypeFK + " INTEGER, " +
                   field_SuppFK + " INTEGER, " + field_status + " TEXT);";
             Cmd.ExecuteNonQuery();
@@ -177,8 +192,8 @@ namespace StoresDatabase
             // create the supplier table
             Cmd.CommandText = "CREATE TABLE IF NOT EXISTS " + table_supplier +
                 " (supID integer primary key, " + field_SupName + " TEXT, " +
-                  field_SupAddress + " TEXT, " + field_Supemail + " TEXT, " + 
-                  field_SupTel + " TEXT);";
+                  field_SupAddress + " TEXT, " + field_Supwebsite + " TEXT" +
+                  field_Supemail + " TEXT, " +  field_SupTel + " TEXT);";
             Cmd.ExecuteNonQuery();
 
             // create parttype table
